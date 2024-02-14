d05387065b399f73abe381a0b2e41ad3ebb06b94
vedhavarsini <vedhavarsini521@gmail.com>
2024-02-14 12:46:41 +0530

    commit

--- IntegratingFrontEndAndBackEnd/chess_pro/src/pages/Admin.jsx
+++
@@ -1,6 +0,0 @@
-function Admin(){
-    return(
-        <h1>Admin page</h1>
-    )
-}
-export default Admin;
--- IntegratingFrontEndAndBackEnd/chess_pro/src/pages/app/store.js
+++
@@ -1,9 +0,0 @@
-import { configureStore } from "@reduxjs/toolkit";
-// import useReducer from "../features/userSlice";
-import useReducer from "../features/userSlice";
-
-export default configureStore({
-  reducer: {
-    user: useReducer,
-  },
-});
